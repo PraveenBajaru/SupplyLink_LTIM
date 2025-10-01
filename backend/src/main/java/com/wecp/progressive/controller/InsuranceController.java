7b8c8c7d2ccf7cec729b9b1a3cfc40e4121f6c65
username <wecp-dummy@gmail.com>
2025-10-01 18:12:25 +0000

    Day11

--- backend/src/main/java/com/wecp/progressive/controller/InsuranceController.java
+++ backend/src/main/java/com/wecp/progressive/controller/InsuranceController.java
@@ -1,21 +1,59 @@
 package com.wecp.progressive.controller;
 
 import com.wecp.progressive.entity.Insurance;
-import org.springframework.http.ResponseEntity;
+import com.wecp.progressive.entity.Shipment;
+import com.wecp.progressive.service.impl.InsuranceServiceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
 import java.util.List;
 
+@RestController
+@RequestMapping("/insurance")
 public class InsuranceController {
+
+    @Autowired
+    InsuranceServiceImpl insuranceServiceImpl;
+
+    @GetMapping
     public ResponseEntity<List<Insurance>> getAllInsurances() {
-        return null;
+        try{
+       return new ResponseEntity<>(insuranceServiceImpl.getAllInsurances(),HttpStatus.OK);
+        }
+        catch(SQLException e)
+    {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     }
 
-    public ResponseEntity<Insurance> getInsuranceById(int insuranceId) {
-        return null;
+    @GetMapping("/{insuranceId}")
+    public ResponseEntity<Insurance> getInsuranceById(@PathVariable int insuranceId) {
+       try{
+        Insurance in=insuranceServiceImpl.getInsuranceById(insuranceId);
+        if(in==null)
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(in,HttpStatus.OK);
+    }
+    catch(SQLException e)
+    {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     }
 
-    public ResponseEntity<Integer> createInsurance(Insurance insurance) {
-        return null;
+    @PostMapping
+    public ResponseEntity<Integer> createInsurance(@RequestBody Insurance insurance) {
+        return new ResponseEntity<>(insuranceServiceImpl.addInsurance(insurance),HttpStatus.CREATED);
     }
 
     public ResponseEntity<Void> updateInsurance(int insuranceId, Insurance insurance) {
